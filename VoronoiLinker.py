0bf9f4b1c8307d2ce8e640be01a34c2b2ac3a2aa
ugorek <120259754+ugorek000@users.noreply.github.com>
2023-05-04 23:16:43 +0700

    Update VoronoiLinker.py

--- VoronoiLinker.py
+++ VoronoiLinker.py
@@ -8,7 +8,7 @@
 
 #Так же надеюсь, что вы простите мне использование только одного файла. 1) Это удобно, всего один файл. 2) До версии 3.5 NodeWrangler так же поставлялся одним файлом.
 
-bl_info = {'name':"Voronoi Linker", 'author':"ugorek", 'version':(2,2,1), 'blender':(3,5,1), #30.04.2023
+bl_?info = {'name':"Voronoi Linker", 'author':"ugorek", 'version':(2,2,1), 'blender':(3,5,1), #30.04.2023
            'description':"Various utilities for nodes connecting, based on the distance field", 'location':"Node Editor > Alt + RMB", 'warning':"", 'category':"Node",
            'wiki_url':"https://github.com/ugorek000/VoronoiLinker/wiki", 'tracker_url':"https://github.com/ugorek000/VoronoiLinker/issues"}
 
